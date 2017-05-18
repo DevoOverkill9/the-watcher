4d2a9b632049a01b568ce6c2afb39e18950d7a64
Dev <devoohk2016@gmail.com>
2017-05-18 06:48:40 +0300

    modifying some terms

--- terms.py
+++ terms.py
@@ -25,19 +25,18 @@ print"""
 can make sniffing on the local network so easy to make it done
 this tool take weeks to make it more easier and flexible for all users so we will be thankful
 if you can rate our tool on github here :
-	https://github.com/DevoOverkill9/the-watcher
+  https://github.com/DevoOverkill9/the-watcher
 Now there is some terms and conditions that maybe helpful and should read:
 {1}
 [1]you can make some edits on this script but you can make us know first we will be 
    thanked if u do this :D
 
-[2]if you found that you can't use this tool you can see our official vedio tutorial on youtube
-
 [3]we are not responsible for any bad using of this tool
-  cause this tool has been made for pentesters and educational purposes only.
+  cause this tool has been made for pentration testers and for educational purposes only.
 
-[4]if you have faced any kind of problems while using our tool you can inform us to our E_mail : 
-	devoohk2016@gmail.com
+[4]if you have faced any kind of problems while using this tool just inform us in issue department on github 
+  here: https://github.com/DevoOverkill9/the-watcher/issues
+
 [5]last thing thanks for reading this and have a nice day :D
 """.format(BLUE , OKGREEN)
 
@@ -48,15 +47,19 @@ try:
 	terms = raw_input("\nThe WATCHER > ")
 	if terms == "y":
 		print("{0}[{1}+{0}]{2}Terms & Conditions has been accepted!").format(WHITE , OKGREEN , BLUE)
+	elif terms == "Y":
+		print("{0}[{1}+{0}]{2}Terms & Conditions has been accepted!").format(WHITE , OKGREEN , BLUE)
 	elif terms == "n":
 		print("{0}[{1}x{0}]{2}Sorry you can't continue if you don't accept our terms!").format(WHITE , RED , RED)
-		os.system(exit)
+		sys.exit(0)
+	elif terms == "N":
+		print("{0}[{1}x{0}]{2}Sorry you can't continue if you don't accept our terms!").format(WHITE , RED , RED)
 	else:
 		print ("\n{}[x]Wrong Entry").format(RED)
-		os.system (exit)
+		sys.exit(0)
 
 except KeyboardInterrupt:
 	print ("""\n{0}[{1}x{0}]{2}Program has been ended
 Thanks for stopping by!
 		""").format(WHITE , RED , RED)
-	os.system(exit)
+	sys.exit(0)
