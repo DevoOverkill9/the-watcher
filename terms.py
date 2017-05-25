e32a40198104fa4f200a3eaf22519593c842af75
Dev <devoohk2016@gmail.com>
2017-05-25 19:36:07 +0300

    Update terms.py
    Animation changes

--- terms.py
+++ terms.py
@@ -1,6 +1,7 @@
 #Terms & Conditions >>
 import os
 import sys
+from time import sleep
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
 HEADER    = '\033[95m'
@@ -31,13 +32,13 @@ Now there is some terms and conditions that maybe helpful and should read:
 [1]you can make some edits on this script but you can make us know first we will be 
    thanked if u do this :D
 
-[2]we are not responsible for any bad using of this tool
+[3]we are not responsible for any bad using of this tool
   cause this tool has been made for pentration testers and for educational purposes only.
 
-[3]if you have faced any kind of problems while using this tool just inform us in issue department on github 
+[4]if you have faced any kind of problems while using this tool just inform us in issue department on github 
   here: https://github.com/DevoOverkill9/the-watcher/issues
 
-[4]last thing thanks for reading this and have a nice day :D
+[5]last thing thanks for reading this and have a nice day :D
 """.format(BLUE , OKGREEN)
 
 print("\n{0}>{1}Do you accept these terms & conditions? {2}({3}y{4}/{5}n{2}) {0}:").format(WHITE , WARNING , WHITE , GREEN , WHITE ,WARNING)
@@ -45,17 +46,15 @@ print("\n{0}>{1}Do you accept these terms & conditions? {2}({3}y{4}/{5}n{2}) {0}
 try:
 	print ("{}").format(BLUE)
 	terms = raw_input("\nThe WATCHER > ")
-	if terms == "y":
-		print("{0}[{1}+{0}]{2}Terms & Conditions has been accepted!").format(WHITE , OKGREEN , BLUE)
-	elif terms == "Y":
-		print("{0}[{1}+{0}]{2}Terms & Conditions has been accepted!").format(WHITE , OKGREEN , BLUE)
-	elif terms == "n":
-		print("{0}[{1}x{0}]{2}Sorry you can't continue if you don't accept our terms!").format(WHITE , RED , RED)
+	if terms == "y" or "Y":
+		print("{0}[{1}+{0}]{2}Terms & Conditions has been accepted!").format(WHITE , OKGREEN , OKBLUE)
+		print("\n{0}[{1}*{0}]{2}Launching...".format(WHITE , OKGREEN , WARNING))
+		sleep(0.5)
+	elif terms == "n" or "N":
+		print("{0}[{1}x{0}]{2}Sorry you can't continue if you don't accept our terms!").format(WHITE , RED , FAIL)
 		sys.exit(0)
-	elif terms == "N":
-		print("{0}[{1}x{0}]{2}Sorry you can't continue if you don't accept our terms!").format(WHITE , RED , RED)
 	else:
-		print ("\n{}[x]Wrong Entry").format(RED)
+		print ("\n{}[x]Wrong Entry").format(FAIL)
 		sys.exit(0)
 
 except KeyboardInterrupt:
