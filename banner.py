20de4a4a018c14789ffc1056f404ae814b896d35
Dev <devoohk2016@gmail.com>
2017-05-25 19:37:21 +0300

    New simple banner

--- banner.py
+++ banner.py
@@ -20,16 +20,14 @@ purple     ="\033[0;35m"
 
 
 print"""
-					     {0}Welcome to the sniffing tool {3}Version {2}2.0
+	   {0}Welcome to the sniffing tool {3}Version {2}3.0
  
-      {1}M""""""""M {2}dP                   {1}M""MMM""MMM""M {2}           dP            dP                         
-{1}Mmmm  mmmM {2}88                   {1}M  MMM  MMM  M {2}          88            88                         
-{1}MMMM  MMMM {2}88d888b. .d8888b.    {1}M  MMP  MMP  M {2}.d8888b. d8888P .d8888b. 88d888b. .d8888b. 88d888b. 
-{1}MMMM  MMMM {2}88'  `88 88ooood8    {1}M  MM'  MM' .M {2}88'  `88   88   88'  `"" 88'  `88 88ooood8 88'  `88 
-{1}MMMM  MMMM {2}88    88 88.  ...    {1}M  `' . '' .MM {2}88.  .88   88   88.  ... 88    88 88.  ... 88       
-{1}MMMM  MMMM {2}dP    dP `88888P'    {1}M    .d  .dMMM {2}`88888P8   dP   `88888P' dP    dP `88888P' dP       
-{1}MMMMMMMMMM {2}                     {1}MMMMMMMMMMMMMM {2}                                                     
-                                                                                        
+ _____  _             __    __        _         _                 
+/__   \| |__    ___  / / /\ \ \ __ _ | |_  ___ | |__    ___  _ __ 
+  / /\/| '_ \  / _ \ \ \/  \/ // _` || __|/ __|| '_ \  / _ \| '__|
+ / /   | | | ||  __/  \  /\  /| (_| || |_| (__ | | | ||  __/| |   
+ \/    |_| |_| \___|   \/  \/  \__,_| \__|\___||_| |_| \___||_|   
+                                                                  
 
 {1}>>{2}Coded & Developed by : {3}David Younan{2}[{1}DevoOverkill9{2}]
 				
